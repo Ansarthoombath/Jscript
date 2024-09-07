f89e566679467e2f3a6f93080ac8a4f3cd5bb6b5
Ansarthoombath <99644029+Ansarthoombath@users.noreply.github.com>
2024-09-07 23:47:55 +0530

    Add files via upload

---
+++ functions/main.js
@@ -0,0 +1,24 @@
+  // function name(){}
+    // name();
+
+    //function name(parameters){}
+    //name(arguments)
+
+    function makeAtea(powder){
+        console.log("  1. Boil Water ")
+        console.log(`2. Prepare the ${powder}` )
+        console.log("   3. Pour Boiling Water ")
+        console.log("   4. Steep the Tea ")
+        console.log("   5. Remove the Tea Bag/Leaves ")
+        console.log("   6. Add Extras (Optional)  ")
+        console.log("   7. Stir and Serve ")
+    
+        }
+
+        makeAtea("tea")
+        makeAtea("coffee")
+    
+       
+    
+    
+       
