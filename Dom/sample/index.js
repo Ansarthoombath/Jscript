71756e3bb47c8fd51cdba2bee7c79f2d7f39e0fb
Ansarthoombath <99644029+Ansarthoombath@users.noreply.github.com>
2024-10-02 21:51:37 +0530

    Add files via upload

---
+++ Dom/sample/index.js
@@ -0,0 +1,8 @@
+document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML="Ansar"
+document.querySelector(".list a").style.color="red"
+// document.querySelector("h1").style.fontSize="10rem"
+document.querySelector("button").classList.add("invisible")
+// document.querySelector("button").classList.toggle("invisible")
+document.querySelector("h1").classList.add("huge")
+document.querySelector("a").getAttribute("href")
+document.querySelector("a").setAttribute("href","https://www.bing.com")
